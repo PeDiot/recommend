c7a642f1e12e7af8f45276b75299c39a12587142
PeDiot <p.emmanuel.diot@gmail.com>
2025-03-11 16:13:08 +0100

    Enhance query processing with multiple loaders and improved logging

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ import src
 
 
 BATCH_SIZE = None
-
+MIN_TEXT_SIZE = 3
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", "-m", type=str, choices=["item", "query"], required=True)
+    
     return parser.parse_args()
 
 
@@ -48,29 +49,42 @@ def main():
     encoder = src.encoder.FashionCLIPEncoder()
     pc_client = Pinecone(api_key=secrets.get("PINECONE_API_KEY"))
     pc_index = pc_client.Index(src.enums.INDEX_NAME)
-    
-    loader = src.bigquery.load_items if args.mode == "item" else src.bigquery.load_queries
-    user_groups = loader(bq_client, BATCH_SIZE)
 
-    n, n_success, n_inserted = 0, 0, 0
-    
-    for user_id, group in user_groups:
-        if args.mode == "item":
-            dataset = src.models.UserDataset.from_image_rows(user_id, group)
-        else:
-            dataset = src.models.UserDataset.from_text_rows(user_id, group)
-            
-        if dataset:
-            n_inserted_ = process_user_dataset(dataset, args.mode)
-            n_success += min(n_inserted_, 1)
-            n_inserted += n_inserted_
-            n += 1
+    loader_kwargs = {"client": bq_client, "n": BATCH_SIZE}   
+    loaders = []
 
-        print(
-            f"Total users: {n} | "
-            f"Success rate: {n_success / n} | "
-            f"Inserted: {n_inserted}"
-        )
+    if args.mode == "item": 
+        loader = src.bigquery.load_items(**loader_kwargs)
+        loaders.append(loader)
+    
+    else:
+        for from_recommend in [False, True]:
+            loader_kwargs["from_recommend"] = from_recommend
+            loader = src.bigquery.load_queries(**loader_kwargs)
+            loaders.append(loader)
+
+    for loader in loaders:
+        n, n_success, n_inserted = 0, 0, 0
+        
+        for user_id, group in loader:
+            if args.mode == "item":
+                dataset = src.models.UserDataset.from_image_rows(user_id, group)
+            else:
+                dataset = src.models.UserDataset.from_text_rows(user_id, group, min_text_size=MIN_TEXT_SIZE)
+                
+            if dataset:
+                n_inserted_ = process_user_dataset(dataset, args.mode)
+                n_success += min(n_inserted_, 1)
+                n_inserted += n_inserted_
+                n += 1
+
+            success_rate = n_success / n if n > 0 else 0
+
+            print(
+                f"Total users: {n} | "
+                f"Success rate: {success_rate:.2f} | "
+                f"Inserted: {n_inserted}"
+            )
 
 if __name__ == "__main__":
     main()
