292c6e669f39660eb306d033043af104f65bcf25
PeDiot <p.emmanuel.diot@gmail.com>
2025-05-15 10:25:51 +0200

    Refactor user dataset processing to use VectorUserDataset, update embedding handling, and adjust Pinecone index references for improved clarity and functionality

--- main.py
+++ main.py
@@ -6,20 +6,21 @@ import src
 BATCH_SIZE = None
 
 
-def process_user_dataset(dataset: src.models.UserDataset) -> int:
+def process_user_dataset(dataset: src.dataset.VectorUserDataset) -> int:
     try:
         if not dataset.is_valid():
             return 0
 
-        embeddings = encoder.encode_images(dataset.images)
         namespace = dataset.user_id
 
         vectors, bq_rows, supabase_rows = src.pinecone.prepare(
-            dataset.point_ids, dataset.metadata_list, embeddings
+            point_ids=dataset.point_ids,
+            metadata_list=dataset.metadata_list,
+            embeddings=dataset.embeddings,
         )
 
         if not src.pinecone.upload(
-            index=pc_index, vectors=vectors, namespace=namespace
+            index=user_vectors_index, vectors=vectors, namespace=namespace
         ):
             return 0
 
@@ -53,20 +54,27 @@ def main():
     supabase_url = secrets["SUPABASE_URL"]
     supabase_key = secrets["SUPABASE_SERVICE_ROLE_KEY"]
 
-    global bq_client, encoder, pc_index
-    bq_client = src.bigquery.init_client(secrets["GCP_CREDENTIALS"])
-    encoder = src.encoder.FashionCLIPEncoder()
-    pc_client = Pinecone(api_key=secrets.get("PINECONE_API_KEY"))
-    pc_index = pc_client.Index(src.enums.INDEX_NAME)
+    global bq_client, user_vectors_index, items_index
 
-    loader_kwargs = {"client": bq_client, "n": BATCH_SIZE}
-    loader = src.bigquery.load_items(**loader_kwargs)
+    bq_client = src.bigquery.init_client(secrets["GCP_CREDENTIALS"])
+
+    pc_client = Pinecone(api_key=secrets.get("PINECONE_API_KEY"))
+    user_vectors_index = pc_client.Index(src.enums.USER_VECTORS_INDEX_NAME)
+    items_index = pc_client.Index(src.enums.ITEMS_INDEX_NAME)
+
+    loader = src.bigquery.load_items(client=bq_client)
     user_item_index = src.supabase.get_user_item_index(supabase_url, supabase_key)
 
     n, n_success, n_inserted = 0, 0, 0
 
     for user_id, group in loader:
-        dataset = src.models.UserDataset.from_image_rows(user_id, group, user_item_index)
+        dataset = src.dataset.VectorUserDataset.from_bigquery_rows(
+            user_id=user_id,
+            rows=group,
+            fetch_vectors_fn=src.pinecone.fetch_vectors,
+            fetch_vectors_kwargs={"index": items_index},
+            user_item_index=user_item_index,
+        )
 
         if dataset:
             n_inserted_ = process_user_dataset(dataset)
